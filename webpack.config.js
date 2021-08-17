8edb906e7306723c33f2ba88f1fec982f99eb4b8
Sara Saiz <sssantos@minsait.com>
2021-08-17 18:18:53 +0200

    Primer commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebPack = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+
+    mode: 'development',
+
+    output: {
+        clean: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                    minimize: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use: [ MiniCssExtract.loader, 'css-loader' ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPack({
+            title: 'Mi Webpack App',
+            template: './src/index.html',
+            filename: './index.html',
+            inject: 'body'
+        }),
+
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+              { from: "src/assets/", to: "assets/" }
+              
+            ],
+         })
+    ]
+}
